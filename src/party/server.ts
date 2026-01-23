19fb6b28279eaef0807cdd2b312d658b1ef22cd9
Sun Envidiado <sun.adriann@icloud.com>
2026-01-23 11:37:16 +0800

    feat: ✨ add CORS support for OPTIONS requests and include PartyKit URL in environment variables

--- src/party/server.ts
+++ src/party/server.ts
@@ -4,11 +4,33 @@ export default class Server implements Party.Server {
 	constructor(public room: Party.Room) {}
 
 	async onRequest(req: Party.Request) {
+		if (req.method === "OPTIONS") {
+			return new Response(null, {
+				status: 204,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+					"Access-Control-Allow-Headers": "Content-Type",
+				},
+			});
+		}
+
 		if (req.method === "POST") {
 			const body = (await req.json()) as { message: string };
 			this.room.broadcast(body.message);
-			return new Response("Broadcasted", { status: 200 });
+
+			return new Response("Broadcasted", {
+				status: 200,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+				},
+			});
 		}
+
+		if (req.method === "GET") {
+			return new Response("PartyKit Server is Running!", { status: 200 });
+		}
+
 		return new Response("Not found", { status: 404 });
 	}
 }
