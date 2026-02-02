8c1a55befe0d42c73b63c047e9774a548236e5e0
Sun <sun.adriann@icloud.com>
2026-02-02 22:56:26 +0800

    feat: ✨ add bullet list functionality to the compact markdown editor and rich text renderer

--- src/components/ui/content/rich-text-renderer.tsx
+++ src/components/ui/content/rich-text-renderer.tsx
@@ -24,7 +24,7 @@ export const RichTextRenderer = ({
 
 	return (
 		<div
-			className={`prose prose-invert prose-sm max-w-none [&_p]:mb-0 [&_p]:leading-normal ${className}`}
+			className={`prose prose-invert prose-sm max-w-none [&_p]:mb-0 [&_p]:leading-normal [&_ul]:list-disc [&_ol]:list-decimal [&_ul]:pl-[1.5em] [&_ol]:pl-[1.5em] [&_li]:pl-0 [&_ul]:mt-[1em] [&_ol]:mt-[1em] ${className}`}
 			dangerouslySetInnerHTML={{ __html: html }}
 		/>
 	);
